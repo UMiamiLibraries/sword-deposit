a7558625108fe11c30ca2f0abb3352ff956eedaa
enprieto <eprieto.n@gmail.com>
2020-03-25 14:47:38 -0400

    v3.1 parameters.py

---
+++ parameters.py
@@ -0,0 +1,57 @@
+dissertation = {
+    'degreename': {
+        'EDD': 'Doctor of Education (EDD)',
+        'DA': 'Doctor of Arts (DA)',
+        'DMA': 'Doctor of Musical Arts (DMA)',
+        'PhD': 'Doctor of Philosophy (PHD)'
+    },
+    'degreetype': {
+        'dissertation': 'Dissertation',
+        'doctoralessay': 'Doctoral Essay',
+        'doctoraltreatise': 'Doctoral Treatise',
+        'lecturerecitalessay': 'Lecture Recital Essay'
+    }
+}
+#
+languages = {
+    'EN': 'English'
+}
+#masters needs to be updated
+masters = {
+    'degreename': {
+        'EDD': 'Doctor of Education (EDD)',
+        'DA': 'Doctor of Arts (DA)',
+        'DMA': 'Doctor of Musical Arts (DMA)',
+        'PhD': 'Doctor of Philosophy (PHD)'
+    },
+    'degreetype': {
+        'dissertation': 'Dissertation',
+        'doctoralessay': 'Doctoral Essay',
+        'doctoraltreatise': 'Doctoral Treatise',
+        'lecturerecitalessay': 'Lecture Recital Essay'
+    }
+}
+#
+departments = {
+
+}
+topics = {
+    'Fine and Performing Arts': {
+        'Art criticism': '0365',
+        'Art history': '0377',
+        'Cinematography': '0435',
+        'Dance': '0378',
+        'Design': '0389',
+        'Fashion': '0200',
+        'Film studies': '0900',
+        'Fine arts': '0357',
+        'Music': '0413',
+        'Music history': '0210',
+        'Music theory': '0223',
+        'Musical composition': '0216',
+        'Musical performances': '0943',
+        'Performing arts': '0641',
+        'Theater': '0465',
+        'Theater history': '0644'
+    }
+}
