7043cc468558f71050afaa0924a7edc486ccd435
Eddie Prieto <eprieto.n@gmail.com>
2021-02-11 13:16:40 -0500

    added ckeditor

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -1,2 +1,4 @@
 from flask import Flask  # Import the Flask class
+from flask_ckeditor import CKEditor
 app = Flask(__name__)    # Create an instance of the class for our use
+ckeditor = CKEditor(app)
---
+++ hello_app/static/ckeditor/config.js
@@ -0,0 +1,85 @@
+/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+
+	// The toolbar groups arrangement, optimized for a single toolbar row.
+	config.extraPlugins = 'wordcount,notification';
+	config.toolbarGroups = [
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'forms' },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'tools' },
+		{ name: 'others' },
+		{ name: 'about' }
+	];
+
+    config.forcePasteAsPlainText = true;
+
+    config.removePlugins = 'pastefromword';
+
+	// The default plugins included in the basic setup define some buttons that
+	// are not needed in a basic editor. They are removed here.
+	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,NumberedList,BulletedList,Link,Unlink,About';
+
+	// Dialog windows are also simplified.
+	config.removeDialogTabs = 'link:advanced';
+
+	config.wordcount = {
+
+        // Whether or not you want to show the Paragraphs Count
+        showParagraphs: false,
+
+        // Whether or not you want to show the Word Count
+        showWordCount: false,
+
+        // Whether or not you want to show the Char Count
+        showCharCount: true,
+
+        // Whether or not you want to count Spaces as Chars
+        countSpacesAsChars: false,
+
+        // Whether or not to include Html chars in the Char Count
+        countHTML: false,
+
+        // Whether or not to include Line Breaks in the Char Count
+        countLineBreaks: false,
+
+        // Maximum allowed Word Count, -1 is default for unlimited
+        maxWordCount: -1,
+
+        // Maximum allowed Char Count, -1 is default for unlimited
+        maxCharCount: 1800,
+
+        // Maximum allowed Paragraphs Count, -1 is default for unlimited
+        maxParagraphs: -1,
+
+        // How long to show the 'paste' warning, 0 is default for not auto-closing the notification
+        pasteWarningDuration: 5000,
+
+
+        // Add filter to add or remove element before counting (see CKEDITOR.htmlParser.filter), Default value : null (no filter)
+        filter: new CKEDITOR.htmlParser.filter({
+            elements: {
+                div: function( element ) {
+                    if(element.attributes.class == 'mediaembed') {
+                        return false;
+                    }
+                }
+            }
+        })
+    };
+
+}
