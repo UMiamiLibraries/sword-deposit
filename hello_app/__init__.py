5433c1a09323a4489484e57fced84fc06516b5b3
cbrownroberts <charlesbrownroberts@miami.edu>
2021-03-03 16:41:21 -0500

    removed dupe

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -2,6 +2,4 @@ from flask import Flask  # Import the Flask class
 from flask_ckeditor import CKEditor
 app = Flask(__name__)    # Create an instance of the class for our use
 ckeditor = CKEditor(app)
-
-app = Flask(__name__) # Create an instance of the class for our use
 mail = Mail(app)
