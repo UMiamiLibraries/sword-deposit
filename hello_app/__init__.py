8772dec7c3769cb3fa9d409f54514eae6acaf5e1
Charles <cgb37@miami.edu>
2021-03-12 15:39:46 -0500

    update with email

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask  # Import the Flask class
 from flask_ckeditor import CKEditor
+from flask_mail import Mail
 app = Flask(__name__)    # Create an instance of the class for our use
 ckeditor = CKEditor(app)
 mail = Mail(app)
